fdd7dfe5524f0455c0e78432e1792661c9acddf6
yadav-khushi293 <yadavkhushi293@gmail.com>
2025-11-29 23:11:51 +0530

    code

--- JS/Index.js
+++ JS/Index.js
@@ -43,3 +43,59 @@ function moveSlider(direction) {
         }
     }, {once: true}) // Remove the event listener after it's triggered once
 }
+
+// SCROLL_IMG_OVSEFLOW_CODE
+
+const api = 'https://khushi-uedn.onrender.com/video';
+
+const ApiCall = () => {
+  fetch(api)
+    .then((res) => res.json())
+    .then((res) => appendsFunc(res))
+    .catch((err) => console.log(err));
+};
+
+const appendsFunc = (data) => {
+  let dataShow = document.getElementById('info');
+
+  data.forEach((el) => {
+    let main_div = document.createElement('div')
+    let cardDiv = document.createElement('div');
+    let videoWrap = document.createElement('div');
+    let video = document.createElement('video');
+     
+    let titleoffer = document.createElement('div');
+    let title = document.createElement('p');
+    let price = document.createElement('p');
+    let offer = document.createElement('p');
+
+    // Classes
+    main_div.classList="main_div";
+    cardDiv.className = "cardDiv";
+    videoWrap.className = "video_1";
+    titleoffer.className = "titleoffer";
+    title.className = "title";
+    price.className = "price";
+    offer.className = "offer";
+
+    // Video URL (important)
+    video.src = el.img;   // ✔ Correct key: "img"
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+    video.width = 250;
+
+    // Text
+    title.innerText = el.title;
+    price.innerText = el.price;
+    offer.innerText = el.offer;
+
+    // Build structure
+    titleoffer.append(title, price, offer);
+    videoWrap.append(video);
+    main_div.append(videoWrap, titleoffer)
+    cardDiv.append(main_div);
+
+    dataShow.append(cardDiv);
+  });
+};
